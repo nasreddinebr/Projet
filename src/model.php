c70e38678cff40265effb36f5b3c88c8528b2d2a
nasreddinebr <berrachednasr@gmail.com>
2017-05-14 18:36:55 +0200

    Remove model.php file

--- src/model.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Return all articles
-function getPosts() {
-	// Data access
-	$db= new PDO('mysql:host=localhost;dbname=blog_ecrivain;charset=utf8', 'nasre', 'gBc546br35*lm');
-	
-	$posts = $db->query('select * from post order by id_post desc');
-	return $posts;
-}
