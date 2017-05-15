35805ee8796125e8fc22a3a50207f169f77d2a5e
nasreddinebr <berrachednasr@gmail.com>
2017-05-15 19:11:32 +0200

    CSS framework integration materialize
    * Using a new template
    * Update your homepage
    * Display the last three article

--- app/app.php
+++ app/app.php
@@ -13,6 +13,7 @@ ExceptionHandler::register();
 // Rgister service provider.
 $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views',));
+$app->register(new Silex\Provider\AssetServiceProvider(), array('assets.version' => 'v1'));
 
 
 // Register services.
