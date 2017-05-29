6d69381a7e0287ce92e11be1f2fb60007b10740a
nasreddinebr <berrachednasr@gmail.com>
2017-05-29 15:34:12 +0200

    Update FileType for upload images

--- src/Form/Type/PostWrite.php
+++ src/Form/Type/PostWrite.php
@@ -4,12 +4,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 class PostWrite extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder
 				->add('title', TextType::class)
 				->add('content', TextareaType::class, array('required' => false,))
+				//->add('attachment', FileType::class, array('required' => false,))
 				->add('publish', CheckboxType::class, array('required' => false,));
 	}
 	
