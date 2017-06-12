7184d9f24b45e61acab24d365e5131fe41c57147
nasreddinebr <berrachednasr@gmail.com>
2017-06-12 15:33:48 +0200

    Update a DataBase file

--- db/database.sql
+++
@@ -1,4 +0,0 @@
-create database if not exists blog_ecrivain character set utf8 collate utf8_general_ci;
-use blog_ecrivain;
-
-grant all privileges on blog_ecrivain.* to 'nasre'@'localhost' identified by 'gBc546br35*lm';
