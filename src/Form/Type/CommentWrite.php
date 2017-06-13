9666c07a679b339f2d798df22e26612ce9cf5ca6
nasreddinebr <berrachednasr@gmail.com>
2017-06-13 04:32:15 +0200

    Add answers to parent comment
    * Add a hidden field to pass the value to parent_id
    * Add the jQuery function that will move the repository form and assign
    the naiccessory value in init.js

--- src/Form/Type/CommentWrite.php
+++ src/Form/Type/CommentWrite.php
@@ -4,10 +4,15 @@ namespace BlogEcrivain\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class CommentWrite extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-		$builder->add('content', TextareaType::class);
+		$builder
+				->add('parent_id', HiddenType::class, array(
+						'data' => 'abcdef',
+				))
+				->add('content', TextareaType::class);
 	}
 	
 	public function getUser() {
--- web/js/init.js
+++ web/js/init.js
@@ -16,5 +16,20 @@ $(document).ready(function(){
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
 });
-        
-        
+
+//Move the form to add a comment to reply to a comment
+jQuery(document).ready(function($){
+	
+	$('.reply').click(function(e){
+		e.preventDefault();
+		var $form = $('#form-comment');
+		var $this = $(this);
+		var parent_id = $this.data('id');
+		var $comment = $('#comment' + parent_id);
+		
+		$form.find('label').text('Réppondre à ce commentaire');
+		$('#comment_write_parent_id').val(parent_id);
+		$comment.after($form);
+	})
+});
+   
